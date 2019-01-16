ec08ef83400e9832f7bd1e25b510770b9cc9c8a3
Stewan <oi@stewan.io>
2019-01-16 16:19:07 -0200

    feat(Photo/ios): save gif to album

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,7 @@ import {
   FileTransferObject
 } from "@ionic-native/file-transfer/ngx";
 import { File } from "@ionic-native/file/ngx";
+import { LoadingController } from "@ionic/angular";
 
 @Component({
   selector: "app-home",
@@ -17,39 +18,115 @@ import { File } from "@ionic-native/file/ngx";
   styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  public albumName: string = `AWESOME`;
+
   // video
-  // public url: string = `https://cdn-dev.inflcr.com/Event/10252/conversions/IMG_3191-small.mp4?response-content-disposition=attachment%3Bfilename%3DIMG_3191-small.mp4&Expires=1547645921&Signature=mtFB-9Epk08iucLE~tpxH8p3UgKBhn8xijfiDIiRUzpqZ883KKyZr6Ix4XTiJNjnp7oIfSPckpCxxarN38MKCrRi5OcAK~wOIDsqFz6NyPSaUdjiNyuk9T4DjIaqa4VKFLsutUc8O2zk2K3s6SHPhAOly~-xDRCZIJKGbUTImfl4i~VuYUcF3VyW3CSi6FEcTglJH9TV8cME8IbB0ZrMKn7hdI7maNtMG4qjqGo9npz-~zqc5gOuG6EFg~lugCA-BCrRu5dJYJUGB6wDiIQ4Vlv6sCPClmSgDAjVryTLPPVooQmzI7kqjKqdOUqyJFiD3JftTlIkkLCaukChqrL1gg__&Key-Pair-Id=APKAJ5AKQ3EML2QIWOJA`;
+  public videoUrl: string =
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4";
+  public videoUrlPoster: string =
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg";
 
   // gif
-  public url: string = `https://cdn-dev.inflcr.com/Event/10053/giphy.gif?response-content-disposition=attachment%3Bfilename%3Dgiphy.gif&Expires=1547659519&Signature=cSZzSxLqQ5VyD0eSPaCQE5j9x~j0LKhS8z5A~38zAz8btSNUDoZFvsDPAPySbn9QVMGY69xoGQQncrGvN1lnxf7rJJUGAxt2QGVyxMka8cmE36AV5IdI-3ZwmJf4GmrMfubVELCfio4HO9gTLCeQUGJHsWMRRtgT5c9DfHN3C9rARsA25fbzg2W8QJBrzgWeNUzywC~60i0SZMSWkK25et~shrKKXjIicHhMXP8yymbpOFUgGhKTldMrc0rWTEirTLtu4A5zK13MaOQQCKUyvi1BYyYx5W-mcsfFC4uSFm1GwV6vYK7ObjveB0CxacTYFtkSmABoCj4o3mbaWu9D4Q__&Key-Pair-Id=APKAJ5AKQ3EML2QIWOJA`;
+  public gifUrl: string = `https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif`;
 
-  constructor(private fileTransfer: FileTransfer, private file: File) {}
+  constructor(
+    private fileTransfer: FileTransfer,
+    private file: File,
+    private loading: LoadingController
+  ) {}
 
-  save() {
+  async saveVideo() {
+    const loader = await this.loading.create({
+      showBackdrop: true,
+      backdropDismiss: true
+    });
     const fileTransfer: FileTransferObject = this.fileTransfer.create();
     const path = this.file.dataDirectory + "my-video.mp4";
-    fileTransfer
-      .download(this.url, path, true)
-      .then(async r => {
-        Plugins.Photos.getAlbums().then(async albums => {
-          const album = albums.albums.filter(
-            album => album.name.toUpperCase() === "INFLCR"
-          )[0];
 
-          Plugins.Photos.saveVideo({
-            data: r.nativeURL,
-            albumIdentifier: album && album.identifier
+    loader.present();
+
+    fileTransfer
+      .download(this.videoUrl, path, true)
+      .then(async r => {
+        let album: any = await this.getAlbum().catch(err =>
+          alert(`Cant get album`)
+        );
+
+        if (!album) {
+          await this.createAlbum().catch(err => alert(`Cant create album`));
+          album = await this.getAlbum().catch(err => alert(`Cant get album`));
+        }
+        Plugins.Photos.saveVideo({
+          data: r.nativeURL,
+          albumIdentifier: album && album.identifier
+        })
+          .then(r => {
+            loader.dismiss();
+            console.log(r);
+            alert(`success, video saved`);
           })
-            .then(r => {
-              console.log(r);
-              alert(`success`);
-            })
-            .catch(err => {
-              console.log(err);
-              alert(`err`);
-            });
-        });
+          .catch(err => {
+            loader.dismiss();
+            console.log(err);
+            alert(`err`);
+          });
       })
       .catch(console.log);
+  }
+
+  async saveGif() {
+    const loader = await this.loading.create({ showBackdrop: true });
+    const fileTransfer: FileTransferObject = this.fileTransfer.create();
+    const path = this.file.dataDirectory + "my-video.gif";
+    loader.present();
+
+    fileTransfer
+      .download(this.gifUrl, path, true)
+      .then(async r => {
+        let album: any = await this.getAlbum().catch(err =>
+          alert(`Cant get album`)
+        );
+
+        if (!album) {
+          await this.createAlbum().catch(err => alert(`Cant create album`));
+          album = await this.getAlbum().catch(err => alert(`Cant get album`));
+        }
+
+        Plugins.Photos.saveGif({
+          data: r.nativeURL,
+          albumIdentifier: album && album.identifier
+        })
+          .then(r => {
+            loader.dismiss();
+            console.log(r);
+            alert(`success! gif saved`);
+          })
+          .catch(err => {
+            loader.dismiss();
+            console.log(err);
+            alert(`err`);
+          });
+      })
+      .catch(console.log);
+  }
+
+  async createAlbum() {
+    let ret = await Plugins.Photos.createAlbum({
+      name: this.albumName
+    });
+    console.log("album created");
+  }
+
+  async getAlbum() {
+    return new Promise((resolve, reject) => {
+      Plugins.Photos.getAlbums()
+        .then(async albums => {
+          const album = albums.albums.filter(
+            album => album.name.toUpperCase() === this.albumName
+          )[0];
+          resolve(album);
+        })
+        .catch(reject);
+    });
   }
 }
