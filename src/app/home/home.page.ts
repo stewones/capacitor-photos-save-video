0e3bb231dc00370b4f8f3eb43435528f5aea3867
Stewan <oi@stewan.io>
2019-03-18 19:17:44 -0300

    chore(): add use case for android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,11 +11,12 @@ import {
 } from "@ionic-native/file-transfer/ngx";
 import { File } from "@ionic-native/file/ngx";
 import { LoadingController } from "@ionic/angular";
-import {
-  Downloader,
-  DownloadEventData,
-  ProgressEventData
-} from "capacitor-downloader";
+
+// import {
+//   Downloader,
+//   DownloadEventData,
+//   ProgressEventData
+// } from "capacitor-downloader";
 
 @Component({
   selector: "app-home",
@@ -23,16 +24,20 @@ import {
   styleUrls: ["home.page.scss"]
 })
 export class HomePage {
-  downloadManager: Downloader = new Downloader();
+  // downloadManager: Downloader = new Downloader();
 
   public albumName: string = `MY AWESOME ALBUM`;
 
   public videoUrl: string =
-    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4";
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4";
+  //"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"; // a bigger video
+
   public videoUrlPoster: string =
     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg";
 
   public gifUrl: string = `https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif`;
+
+  public photoUrl: string = `https://www.humanesociety.org/sites/default/files/2018/06/cat-217679.jpg`;
 
   constructor(
     private fileTransfer: FileTransfer,
@@ -43,8 +48,12 @@ export class HomePage {
     // Plugins.Photos.createAlbum({ name: this.albumName })
     //   .then(r => console.log("success response", r))
     //   .catch(err => console.log("ERROR", err));
+  }
 
-    this.saveGif();
+  ngOnInit() {
+    // setTimeout(() => {
+    //   this.saveGif();
+    // }, 5000);
   }
 
   async saveVideo() {
@@ -60,17 +69,19 @@ export class HomePage {
     fileTransfer
       .download(this.videoUrl, path, true)
       .then(async r => {
-        let album: any = await this.getAlbum().catch(err =>
-          alert(`Cant get album`)
-        );
+        // album handler (only ios)
+        // let album: any = await this.getAlbum().catch(err =>
+        //   alert(`Cant get album`)
+        // );
 
-        if (!album) {
-          await this.createAlbum().catch(err => alert(`Cant create album`));
-          album = await this.getAlbum().catch(err => alert(`Cant get album`));
-        }
+        // if (!album) {
+        //   await this.createAlbum().catch(err => alert(`Cant create album`));
+        //   album = await this.getAlbum().catch(err => alert(`Cant get album`));
+        // }
         Plugins.Photos.saveVideo({
           data: r.nativeURL,
-          albumIdentifier: album && album.identifier
+          albumIdentifier: this.albumName
+          // albumIdentifier: album && album.identifier
         })
           .then(r => {
             loader.dismiss();
@@ -83,11 +94,16 @@ export class HomePage {
             alert(`err`);
           });
       })
-      .catch(console.log);
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   async saveGif() {
-    const loader = await this.loading.create({ showBackdrop: true });
+    const loader = await this.loading.create(<any>{
+      showBackdrop: true,
+      backdropDismiss: true
+    });
     const fileTransfer: FileTransferObject = this.fileTransfer.create();
     const path = this.file.dataDirectory + "my-video.gif";
     loader.present();
@@ -95,18 +111,23 @@ export class HomePage {
     fileTransfer
       .download(this.gifUrl, path, true)
       .then(async r => {
-        let album: any = await this.getAlbum().catch(err =>
-          alert(`Cant get album`)
-        );
-        console.log(`ALBUM NAME`, album);
-        if (!album) {
-          await this.createAlbum().catch(err => alert(`Cant create album`));
-          album = await this.getAlbum().catch(err => alert(`Cant get album`));
-        }
+        loader.dismiss();
+
+        // let album: any =
+        //   (await this.getAlbum().catch(err => alert(`Cant get album`))) ||
+        //   this.albumName;
+
+        // if (!album) {
+        //   await this.createAlbum().catch(err => alert(`Cant create album`));
+        //   album = await this.getAlbum().catch(err => alert(`Cant get album`));
+        // }
+        //  alert(r.nativeURL);
+        // console.log(`ALBUM NAME`, album, "NATIVE URL", r.nativeURL);
 
         Plugins.Photos.saveGif({
           data: r.nativeURL,
-          albumIdentifier: (album && album.identifier) || album
+          albumIdentifier: this.albumName
+          // albumIdentifier: (album && album.identifier) || album
         })
           .then(r => {
             loader.dismiss();
@@ -119,7 +140,75 @@ export class HomePage {
             alert(`err`);
           });
       })
-      .catch(err => console.log("FILE TRANSFER ERROR", err));
+      .catch(err => {
+        console.log("FILE TRANSFER ERROR", err);
+        loader.dismiss();
+      });
+
+    // const data = await this.downloadManager.createDownload({
+    //   url: this.gifUrl
+    //   //'https://wallpaperscraft.com/image/hulk_wolverine_x_men_marvel_comics_art_99032_3840x2400.jpg'
+    // });
+    // const imageDownloaderId = data.value;
+    // this.downloadManager.initialize();
+    // this.downloadManager
+    //   .start({ id: imageDownloaderId }, (progressData: ProgressEventData) => {
+    //     console.log(`Progress : ${progressData.value}%`);
+    //     console.log(`Current Size : ${progressData.currentSize}%`);
+    //     console.log(`Total Size : ${progressData.totalSize}%`);
+    //     console.log(`Download Speed in bytes : ${progressData.speed}%`);
+    //   })
+    //   .then((completed: DownloadEventData) => {
+    //     console.log(`Image : ${completed.path}`);
+    //   })
+    //   .catch(err => console.log("FILE TRANSFER ERROR", err));
+  }
+
+  async savePhoto() {
+    const loader = await this.loading.create(<any>{
+      showBackdrop: true,
+      backdropDismiss: true
+    });
+    const fileTransfer: FileTransferObject = this.fileTransfer.create();
+    const path = this.file.dataDirectory + "my-photo.jpg";
+    loader.present();
+
+    fileTransfer
+      .download(this.photoUrl, path, true)
+      .then(async r => {
+        loader.dismiss();
+
+        // let album: any =
+        //   (await this.getAlbum().catch(err => alert(`Cant get album`))) ||
+        //   this.albumName;
+
+        // if (!album) {
+        //   await this.createAlbum().catch(err => alert(`Cant create album`));
+        //   album = await this.getAlbum().catch(err => alert(`Cant get album`));
+        // }
+        //  alert(r.nativeURL);
+        // console.log(`ALBUM NAME`, album, "NATIVE URL", r.nativeURL);
+
+        Plugins.Photos.savePhoto({
+          data: r.nativeURL,
+          albumIdentifier: this.albumName
+          // albumIdentifier: (album && album.identifier) || album
+        })
+          .then(r => {
+            loader.dismiss();
+            console.log(r);
+            alert(`success! jpg saved`);
+          })
+          .catch(err => {
+            loader.dismiss();
+            console.log(err);
+            alert(`err`);
+          });
+      })
+      .catch(err => {
+        console.log("FILE TRANSFER ERROR", err);
+        loader.dismiss();
+      });
 
     // const data = await this.downloadManager.createDownload({
     //   url: this.gifUrl
@@ -151,10 +240,11 @@ export class HomePage {
     return new Promise((resolve, reject) => {
       Plugins.Photos.getAlbums()
         .then(async albums => {
-          const album = albums.albums.filter(
-            album => album.name.toUpperCase() === this.albumName
-          )[0];
-          resolve(album);
+          console.log(albums.albums[0]);
+          // const album = albums.albums.filter(
+          //   album => album.name.toUpperCase() === this.albumName
+          // )[0];
+          // resolve(album);
         })
         .catch(reject);
     });
